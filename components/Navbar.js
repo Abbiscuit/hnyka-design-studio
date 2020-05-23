9b185db8cfea0fcbf4e1952bcc5a81f80b572f87
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-05-23 23:15:46 +0900

    feat(Navbar): Add Navbar styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const Div = styled.div`
-  padding: 8px;
-`;
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <Div>
-      <p>hello</p>
+      <Ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+      </Ul>
     </Div>
   );
 };
 
 export default Navbar;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 8px 10px;
+  height: 64px;
+  background-color: #fff;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 32px;
+`;
+
+const Ul = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: 50%;
+  li {
+    a {
+      color: ${({ theme }) => theme.colors.black};
+      text-decoration: none;
+      padding: 8px;
+      font-size: 12px;
+    }
+  }
+`;

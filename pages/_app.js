a6bd3e8ef93b283f0ef2f785d3105b0d8edd0e7c
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-05-23 22:45:10 +0900

    feat: Add basic components for styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
-const MyApp = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Component {...pageProps} />
-    </>
-  );
-};
+import App from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
 
 export default MyApp;
